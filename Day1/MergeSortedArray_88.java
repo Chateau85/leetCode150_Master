23aadd5ff772ed895fa279d250af6a3cbc812b6b
Cheolwoo Nam <545i@naver.com>
2025-12-30 17:59:50 +0900

    LeetCode150 마스터하기 Day1

---
+++ Day1/MergeSortedArray_88.java
@@ -0,0 +1,50 @@
+package Day1;
+// 
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+// Example 1:
+
+// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+// Output: [1,2,2,3,5,6]
+// Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+// The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+// Example 2:
+
+// Input: nums1 = [1], m = 1, nums2 = [], n = 0
+// Output: [1]
+// Explanation: The arrays we are merging are [1] and [].
+// The result of the merge is [1].
+// Example 3:
+
+// Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+// Output: [1]
+// Explanation: The arrays we are merging are [] and [1].
+// The result of the merge is [1].
+// Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1; // nums1 유효 데이터 끝
+        int p2 = n - 1; // nums2 유효 데이터 끝
+        int p = m + n - 1; // 값을 채워넣을 nums1 가장 끝 위치
+
+        // 두 배열 중 하나라도 빌 때까지 반복
+        while (p1 >=0 && p2 >= 0) {
+            if(nums1[p1] > nums2[p2]) {
+                nums1[p--] = nums1[p1--]; // nums1이 더 크면 뒤에 배치
+            } else {
+                nums1[p--] = nums2[p2--];
+            }
+        }
+
+        // nums2에 남은 데이터가 있다면 다 옮김
+        // nums1에 남은 건 이미 제자리에 있음
+        while (p2 >= 0) {
+            nums1[p--] = nums2[p2--];
+        }
+    }
+}
